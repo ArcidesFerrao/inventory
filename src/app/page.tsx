2f2902f6e2c08e5aaf56534d8dd4e53a9295e1ce
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-27 15:47:36 +0200

    home page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p>login</p>
-    </div>
+    <main className="">
+      <div>
+        <h1>Welcome to Innovante</h1>
+        <p>Please login to continue!</p>
+      </div>
+      <Link href="/api/auth/signin">Login</Link>
+    </main>
   );
 }
