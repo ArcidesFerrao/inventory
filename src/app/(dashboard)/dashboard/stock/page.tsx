d96b0d4c1d0a1644546ee51a363bfc540bd656b9
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-31 18:43:19 +0200

    stock page

--- src/app/(dashboard)/dashboard/stock/page.tsx
+++ src/app/(dashboard)/dashboard/stock/page.tsx
@@ -1,5 +1,57 @@
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-export default function StockPage() {
-  return <div>StockPage</div>;
+export default async function StockPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user)
+    return (
+      <section>
+        <div>
+          <p>User not found!</p>
+        </div>
+      </section>
+    );
+
+  const products = await db.product.findMany({
+    where: { userId: session.user.id },
+  });
+
+  return (
+    <section className="flex flex-col gap-4">
+      <h2 className="text-2xl font-semibold">Stock</h2>
+      <div className="flex gap-2">
+        <h3>Number of Products:</h3>
+        <p>{products.length}</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.stock}</td>
+              <td>
+                {item.stock > 5 ? (
+                  <span className="w-4 h-4 bg-green-600"></span>
+                ) : item.stock > 0 ? (
+                  <span className="w-4 h-4 bg-amber-400"></span>
+                ) : (
+                  <span className="w-4 h-4 bg-red-400"></span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
 }
