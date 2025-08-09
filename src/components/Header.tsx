e97bb6f3e12c74ce7d8e92612376835ae10175e6
ArcidesFerrao <cidesferrao@gmail.com>
2025-08-09 15:54:22 +0200

    user account page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,9 +21,7 @@ export const Header = () => {
         ) : (
           <p className="header-welcome">
             Welcome,{" "}
-            <Link href={`/stock/${session?.user.id}`}>
-              {session?.user.name}
-            </Link>
+            <Link href={`/${session?.user.id}`}>{session?.user.name}</Link>
           </p>
         )}
       </div>
