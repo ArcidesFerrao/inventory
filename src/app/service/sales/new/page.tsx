20927299b685cd52655941665590322135e2f297
ArcidesFerrao <cidesferrao@gmail.com>
2025-08-28 21:46:23 +0200

    sale form with fetched products

--- src/app/service/sales/new/page.tsx
+++ src/app/service/sales/new/page.tsx
@@ -1,13 +1,45 @@
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function NewSale() {
+export default async function NewSale() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) redirect("/login");
+
+  const products = await db.product.findMany({
+    where: { userId: session.user.id },
+  });
+
   return (
     <div className="sales-section flex gap-4 w-full">
       <div>
         <h2>Products</h2>
-        <ul className=" w-96">
+        <ul className=" w-lg">
+          {products.map((product) => (
+            <li
+              key={product.id}
+              className="flex justify-between items-center p-1 border-b"
+            >
+              <h3>{product.name}</h3>
+
+              <div className="flex gap-2 items-center w-46">
+                <div className="amount-btn flex gap-2 items-center px-2 py-1">
+                  <button>-</button>
+                  <span>0</span>
+                  <button>+</button>
+                </div>
+                <span>
+                  <p>{product.price} MZN</p>
+                </span>
+              </div>
+            </li>
+          ))}
           <li className="flex justify-between items-center p-2 border-b">
             <h3>Hamburguer</h3>
+
             <div className="flex gap-2 items-center w-40">
               <div className="flex gap-2 items-center px-2 py-1">
                 <button>-</button>
