d94d9dacc9fa14c3fdef2ab9d007d003a4e6d0a0
ArcidesFerrao <cidesferrao@gmail.com>
2025-09-18 23:57:38 +0200

    min height

--- src/app/service/layout.tsx
+++ src/app/service/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   return (
     <main className="dashboard-main flex gap-5">
       <ServiceNav />
-      <section className="dash-section flex w-full min-h-screen rounded p-8">
+      <section className="dash-section flex w-full rounded p-8">
         {children}
       </section>
     </main>
