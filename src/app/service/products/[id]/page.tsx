b7b3a4fbabddbbee2860c6f050a9678f61a50026
ArcidesFerrao <cidesferrao@gmail.com>
2025-09-19 00:55:16 +0200

    service product page info

--- src/app/service/products/[id]/page.tsx
+++ src/app/service/products/[id]/page.tsx
@@ -14,18 +14,37 @@ export default async function ProductPage(props: { params: Params }) {
     },
   });
   return (
-    <div>
-      <h1>Id: {product?.id}</h1>
-      <h2>Name: {product?.name}</h2>
-      <h2>Price: {product?.price}</h2>
+    <div className="flex flex-col gap-2 items-start">
+      <div className="flex justify-between w-full">
+        <div>
+          <h2 className="text-xl">{product?.name}</h2>
+          <p className="text-xs font-thin">Id: {product?.id}</p>
+        </div>
+        <div className="flex gap-2">
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+      <div className="flex gap-10">
+        <div>
+          {product?.type === "STOCK" && (
+            <div>
+              <h2>Unit: {product?.Unit?.name}</h2>
+              <h2>Quantity: {product?.quantity}</h2>
+              <h2>Stock: {product?.stock ? "Yes" : "No"}</h2>
+              <h2>Cost: {product?.cost}</h2>
+            </div>
+          )}
+          <h2>Price: {product?.price?.toFixed(2)} MZN</h2>
+        </div>
+
+        <div>
+          <h2>Type: {product?.type}</h2>
+          <h2>Category: {product?.Category?.name}</h2>
+          {product?.type === "STOCK" && <h2>Status: {product?.status}</h2>}
+        </div>
+      </div>
       <h2>Description: {product?.description}</h2>
-      <h2>Type: {product?.type}</h2>
-      <h2>Category: {product?.Category?.name}</h2>
-      <h2>Quantity: {product?.quantity}</h2>
-      <h2>Unit: {product?.Unit?.name}</h2>
-      <h2>Stock: {product?.stock ? "Yes" : "No"}</h2>
-      <h2>Cost: {product?.cost || "..."}</h2>
-      <h2>Status: {product?.status}</h2>
       <h2>Created At: {product?.createdAt.toLocaleDateString()}</h2>
       <h2>Updated At: {product?.updatedAt.toLocaleDateString()}</h2>
     </div>
