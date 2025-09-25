2605a46ad4f3df0a023d273ae7da8e0d63781036
ArcidesFerrao <cidesferrao@gmail.com>
2025-09-26 00:04:00 +0200

    supply + service pages access only when registered

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "./lib/db";
 
 const protectedRoutes = ["/dashboard", "/products", "/orders", "/stock", "/service", "/supply", "/[id]"];
 
@@ -11,13 +12,40 @@ export async function middleware(req: NextRequest) {
     const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
 
     if (isProtected && !token) {
-        console.log("User testing mode")
         return NextResponse.redirect(new URL("/login", req.url));
     }
     
+    if (token) {
+        const userId = token.sub;
+
+        if (pathname.startsWith("/supply")) {
+            const supplier = await db.supplier.findUnique({ where: { userId }});
+
+            if (!supplier && pathname !== "/supply/register") {
+                return NextResponse.redirect(new URL("/supply/register", req.url))
+            }
+        }
+
+        if (pathname.startsWith("/service")) {
+            const service = await db.service.findUnique({ where: { userId }})
+
+            if (!service && pathname !== "/service/register") {
+                return NextResponse.redirect(new URL("/service/register", req.url))
+            }
+        }
+    }
+
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard/:path*", "/products/:path*", "/orders/:path*","/stock/:path*","/service/:path*","/supply/:path*", "/:id/:path*", ],
+    matcher: [
+        "/dashboard/:path*", 
+        "/products/:path*", 
+        "/orders/:path*",
+        "/stock/:path*",
+        "/service/:path*",
+        "/supply/:path*", 
+        "/:id/:path*", 
+    ],
 }
