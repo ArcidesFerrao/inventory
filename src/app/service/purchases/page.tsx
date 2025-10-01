e24584784f6583a8f75769c6b14278f36a7506d6
ArcidesFerrao <cidesferrao@gmail.com>
2025-10-01 22:06:35 +0200

    orders placed list

--- src/app/service/purchases/page.tsx
+++ src/app/service/purchases/page.tsx
@@ -22,81 +22,86 @@ export default async function PurchasesPage() {
   });
 
   return (
-    <div className="products-list flex flex-col gap-4 w-full">
-      <div className="list-header flex items-center justify-between w-full">
-        <h2 className="text-2xl font-medium">Recent Purchases</h2>
-
-        <div className="flex gap-4">
+    <div className="flex flex-col gap-5 w-full">
+      <div className="products-list flex flex-col gap-4">
+        <div className="list-header flex items-center justify-between w-full">
+          <h2 className="text-2xl font-medium">Recent Purchases</h2>
           <Link
             href="/service/purchases/new"
             className="add-product flex gap-1"
           >
             <span className="text-md px-2">Purchase</span>
           </Link>
-          <Link
-            href="/service/purchases/orders/new"
-            className="add-product flex gap-1"
-          >
-            <span className="text-md px-2">Order</span>
-          </Link>
+        </div>
+        {purchases.length === 0 ? (
+          <p>No purchases found...</p>
+        ) : (
+          <table className="rounded">
+            <thead>
+              <tr>
+                <th className="text-start">Payment Type</th>
+                <th className="text-start">Total (MZN)</th>
+                <th className="text-start">Date</th>
+                <th className="text-start">Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {purchases.map((purchase) => (
+                <tr key={purchase.id}>
+                  <td>{purchase.paymentType}</td>
+                  <td>{purchase.total}.00</td>
+                  <td>{purchase.date.toLocaleDateString()}</td>
+                  <td>{purchase.date.toLocaleTimeString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div className="flex">
+          <h2 className="text-lg font-bold">Total Purchases:</h2>
+          <p className="text-lg font-bold px-2">
+            MZN {purchases.reduce((acc, sale) => acc + sale.total, 0)}.00
+          </p>
+        </div>
+        <div className="divider"></div>
+        <div className="products-list flex flex-col gap-4">
+          <div className="list-header flex items-center justify-between w-full">
+            <h2 className="text-2xl font-medium">Orders Placed</h2>
+            <Link
+              href="/service/purchases/orders/new"
+              className="add-product flex gap-1"
+            >
+              <span className="text-md px-2">Order</span>
+            </Link>
+          </div>
+          {orders.length === 0 ? (
+            <p>No orders found...</p>
+          ) : (
+            <table className="rounded">
+              <thead>
+                <tr>
+                  <th className="text-start">Payment Type</th>
+                  <th className="text-start">Total (MZN)</th>
+                  <th className="text-start">Deliveries</th>
+                  <th className="text-start">Placed Date</th>
+                  <th className="text-start">Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order) => (
+                  <tr key={order.id}>
+                    <td>{order.paymentType}</td>
+                    <td>{order.total}.00</td>
+                    <td>{order.confirmedDeliveries.length}</td>
+                    <td>{order.createdAt.toLocaleDateString()}</td>
+                    <td>{order.createdAt.toLocaleTimeString()}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
-      {purchases.length === 0 ? (
-        <p>No purchases found...</p>
-      ) : (
-        <table className="rounded">
-          <thead>
-            <tr>
-              <th className="text-start">Payment Type</th>
-              <th className="text-start">Total (MZN)</th>
-              <th className="text-start">Date</th>
-              <th className="text-start">Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {purchases.map((purchase) => (
-              <tr key={purchase.id}>
-                <td>{purchase.paymentType}</td>
-                <td>{purchase.total}.00</td>
-                <td>{purchase.date.toLocaleDateString()}</td>
-                <td>{purchase.date.toLocaleTimeString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      <div className="flex">
-        <h2 className="text-lg font-bold">Total Purchases:</h2>
-        <p className="text-lg font-bold px-2">
-          MZN {purchases.reduce((acc, sale) => acc + sale.total, 0)}.00
-        </p>
-      </div>
-      {orders.length === 0 ? (
-        <p>No orders found...</p>
-      ) : (
-        <table className="rounded">
-          <thead>
-            <tr>
-              <th className="text-start">Payment Type</th>
-              <th className="text-start">Total (MZN)</th>
-              <th className="text-start">Deliveries</th>
-              <th className="text-start">Placed Date</th>
-              <th className="text-start">Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order.id}>
-                <td>{order.paymentType}</td>
-                <td>{order.total}.00</td>
-                <td>{order.confirmedDeliveries.length}</td>
-                <td>{order.createdAt.toLocaleDateString()}</td>
-                <td>{order.createdAt.toLocaleTimeString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
     </div>
   );
 }
