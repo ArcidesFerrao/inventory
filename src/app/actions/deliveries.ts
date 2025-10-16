d16b72c2c25990e2bdb2d42bb6945f2461057ba3
ArcidesFerrao <cidesferrao@gmail.com>
2025-10-16 22:34:16 +0200

    create delivery action

--- src/app/actions/deliveries.ts
+++ src/app/actions/deliveries.ts
@@ -82,33 +82,192 @@ export async function completeDelivery({deliveryId, orderId, supplierOrderId}:{d
     if (!session?.user) redirect("/login");
 
     try {
-        const delivery = await db.delivery.update({
-            where: {
-                id: deliveryId,
-            },
-            data: {
-                status: "COMPLETED",
-            }})
-        const suppplierOrder = await db.supplierOrder.update({
-            where: {
-                id: supplierOrderId,
-            },
-            data: {
-                status: "COMPLETED",
-            }
-        })
-        const order = await db.order.update({
-            where: {
-                id: orderId,
-            },
-            data: {
-                status: "DELIVERED",
-            }
-        })
+        return await db.$transaction(async (tx) => {
+            const [delivery, supplierOrder, order] = await Promise.all([
+                tx.delivery.update({
+                    where: {
+                        id: deliveryId,
+                    },
+                    data: {
+                        status: "COMPLETED",
+                    },
+                    include: {
+                        deliveryItems: {
+                            include: {
+                                orderItem: {
+                                    include: {
+                                        product: true,
+                                        supplierOrder: {
+                                            include: {
+                                                order: {
+                                                    include: {
+                                                        Service: true,
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }),
+                tx.supplierOrder.update({
+                    where: {
+                        id: supplierOrderId,
+                    }, 
+                    data: {
+                        status: "COMPLETED",
+                    }
+                }),
+                tx.order.update({
+                    where: {
+                        id: orderId,
+                    },
+                    data: {
+                        status: "DELIVERED",
+                    },
+                })
+            ])
 
-        return {success: true, delivery, order, suppplierOrder};
+            for (const deliveryItem of delivery.deliveryItems) {
+                const orderItem = deliveryItem.orderItem;
+                const supplierProduct = orderItem.product;
+                // const supplierId = orderItem.supplierOrder.supplierId;
+                const service = orderItem.supplierOrder.order.Service;
+                const quantity = deliveryItem.quantity;
+
+                if (!service) continue;
+
+                const updatedSupplierOrder = await tx.supplierProduct.update({
+                    where: {
+                        id: supplierProduct.id,
+                    }, 
+                    data: {
+                        stock: {
+                            decrement: quantity,
+                        }
+                    }
+                });
+
+                await tx.stockMovement.create({
+                    data: {
+                        productId: supplierProduct.id,
+                        changeType: "SALE",
+                        quantity,
+                        referenceId: deliveryItem.id,
+                        notes: `Delivery #${delivery.id.slice(0,8)}... completed, deducted from supplier stock`,
+                    }
+                })
+
+                let serviceProduct = await tx.product.findFirst({
+                    where: {
+                        serviceId: service.id,
+                        name: {
+                            equals: supplierProduct.name,
+                            mode: "insensitive",
+                        }
+                    }
+                });
+
+                if (serviceProduct) {
+                    await tx.product.update({
+                        where: {
+                            id: serviceProduct.id,
+                        },
+                        data: {
+                            stock: {
+                                increment: quantity,
+                            }
+                        }
+                    })
+                } else {
+                    serviceProduct = await tx.product.create({
+                        data: {
+                            name: supplierProduct.name,
+                            description: supplierProduct.description,
+                            unitQty: supplierProduct.unitQty,
+                            unitId: supplierProduct.unitId,
+                            stock: quantity,
+                            serviceId: service.id,
+                            price: supplierProduct.price,
+                        }
+                    })
+                } 
+                
+                await tx.stockMovement.create({
+                    data: {
+                        productId: serviceProduct?.id,
+                        changeType: "PURCHASE",
+                        quantity,
+                        referenceId: deliveryItem.id,
+                        notes: `Delivery #${delivery.id.slice(0,8)}... completed, added to service stock`,
+                    }
+                })
+
+                await logActivity(
+                    service.id,
+                    updatedSupplierOrder.supplierId,
+                    "DELIVERY_CONFIRMED",
+                    "Delivery",
+                    delivery.id,
+                    `Delivery #${delivery.id} marked as Completed`,
+                    {
+                        deliveryId,
+                        orderId,
+                        supplierOrderId,
+                        totalItems: delivery.deliveryItems.length,
+                        deliveryItems: delivery.deliveryItems
+                    },
+                    null,
+                    'INFO',
+                    null
+                )
+            }
+
+            return { success: true, delivery, supplierOrder, order };
+        })
+        // const delivery = await db.delivery.update({
+        //     where: {
+        //         id: deliveryId,
+        //     },
+        //     data: {
+        //         status: "COMPLETED",
+        //     }})
+        // const suppplierOrder = await db.supplierOrder.update({
+        //     where: {
+        //         id: supplierOrderId,
+        //     },
+        //     data: {
+        //         status: "COMPLETED",
+        //     }
+        // })
+        // const order = await db.order.update({
+        //     where: {
+        //         id: orderId,
+        //     },
+        //     data: {
+        //         status: "DELIVERED",
+        //     }
+        // })
+
+        // return {success: true, delivery, order, suppplierOrder};
     } catch (error) {
         console.error("Error completing delivery:", error);
+        await logActivity(
+                    null,
+                    null,
+                    "DELIVERY_Error",
+                    "Delivery",
+                    deliveryId,
+                    `Error while completing delivery #${deliveryId}`,
+                    {
+                        error: error instanceof Error ? error.message : String(error),
+                    },
+                    null,
+                    'INFO',
+                    null
+                )
         throw new Error("Failed to complete delivery");
     }
 }
