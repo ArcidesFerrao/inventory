3bcd18f7a5c5cdbd053f2e33d27a81cf836b2b61
ArcidesFerrao <cidesferrao@gmail.com>
2025-10-20 21:21:56 +0200

    more info on supplier

--- src/app/service/purchases/orders/new/page.tsx
+++ src/app/service/purchases/orders/new/page.tsx
@@ -18,12 +18,16 @@ export default async function NewOrder() {
     <>
       <div className="sales-section flex flex-col gap-5 w-full">
         <div className="list-header flex items-center justify-between w-full">
-          <h2 className="text-2xl font-bold">Order Products</h2>
+          <h2 className="text-2xl font-bold">Suppliers List</h2>
           <Link href="/service/purchases" className="add-product flex gap-1">
             <span className="text-md px-2">Cancel</span>
           </Link>
         </div>
-        {suppliers.length > 0 && <ServiceOrder suppliers={suppliers} />}
+        {suppliers.length > 0 ? (
+          <ServiceOrder suppliers={suppliers} />
+        ) : (
+          <p>No suppliers found...</p>
+        )}
       </div>
     </>
   );
--- src/components/ServiceOrder.tsx
+++ src/components/ServiceOrder.tsx
@@ -33,21 +33,27 @@ export const ServiceOrder = ({
         >
           <div className="flex justify-between gap-5">
             <button
-              className={
+              className={`flex items-center gap-1 ${
                 toggleSupplier === supplier.id
                   ? ""
                   : "opacity-50 p-4 w-full text-start hover:opacity-70"
-              }
+              }`}
               onClick={() => openSupplier(supplier.id)}
             >
               {supplier.name}
+              <p className="text-sm font-light">- {supplier.description}</p>
             </button>
+            {toggleSupplier !== supplier.id && (
+              <p className=" text-nowrap text-sm opacity-50 font-light  flex items-center gap-1 pr-4 hover:opacity-70">
+                {`${supplier.products.length} products`}
+              </p>
+            )}
             {toggleSupplier === supplier.id && (
               <button
                 onClick={() => closeSupplier()}
-                className="border-full border-blue-100"
+                className="opacity-50 border-full border-blue-100 hover:opacity-75"
               >
-                x
+                <span className="carbon--close-outline"></span>
               </button>
             )}
           </div>
