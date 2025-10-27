0e9ea1fd24700ddf85a9f713ea0d5df77822c19e
ArcidesFerrao <cidesferrao@gmail.com>
2025-10-27 19:19:54 +0200

    admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import React from "react";
 import { getAdminStats } from "../actions/dashboardStats";
+import { Card } from "@/components/Card";
 
 export default async function AdminPage() {
   const session = await getServerSession(authOptions);
@@ -24,94 +25,45 @@ export default async function AdminPage() {
       <div className="admin-header">
         <h1 className="text-4xl font-medium">Admin Dashboard</h1>
       </div>
-      <div className="admin-info flex gap-10">
-        <div className="admin-user flex flex-col gap-5">
-          <h2 className="text-2xl font-bold underline">Users</h2>
-          <div className="flex  gap-5">
-            <div className="flex flex-col ">
-              <div className="flex flex-col gap-2">
-                <div className="flex flex-col gap-1">
-                  <p className="text-lg font-normal">Total</p>
-                  <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                    {stats.totals.totalUsers}
-                  </h4>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <p className="text-lg font-normal">Active</p>
-                  <h4 className="text-xl py-1 whitespace-nowrap font-bold"></h4>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col gap-1">
-                <p className="text-lg font-normal">Services</p>
-                <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                  {stats.totals.totalServices}
-                </h4>
-              </div>
-              <div className="flex flex-col gap-1">
-                <p className="text-lg font-normal">Suppliers</p>
-                <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                  {stats.totals.totalSuppliers}
-                </h4>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="admin-orders flex flex-col gap-2">
-          <h2 className="text-2xl font-bold underline">Orders</h2>
-          <div className="flex gap-5">
-            <div className="flex flex-col gap-1">
-              <p className="text-lg font-normal">Total</p>
-              <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                {stats.totals.totalOrders}
-              </h4>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="text-lg font-normal">Delivered</p>
-              <h4 className="text-xl py-1 whitespace-nowrap font-bold"></h4>
-            </div>
-          </div>
-        </div>
-        <div className="admin-orders flex flex-col gap-2">
-          <h2 className="text-2xl font-bold underline">Products</h2>
-          <div className="flex gap-5">
-            <div className="flex flex-col gap-1">
-              <p className="text-lg font-normal">Total</p>
-              <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                {stats.totals.totalProducts}
-              </h4>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="text-lg font-normal">Sales</p>
-              <h4 className="text-xl py-1 whitespace-nowrap font-bold">
-                {stats.totals.totalSales}
-              </h4>
-            </div>
-          </div>
-        </div>
+      <div className="flex justify-between">
+        <Card title="Total Suppliers" value={stats.totals.totalUsers} />
+        <Card title="Total Services" value={stats.totals.totalServices} />
+        <Card title="Total Orders" value={stats.totals.totalOrders} />
+        <Card title="Total Sales" value={stats.totals.totalSales} />
       </div>
-      <div className="top-stats flex justify-between gap-4">
-        <div className="top-suppliers flex flex-col gap-2">
+      <div className="top-stats grid grid-cols-2 gap-5 ">
+        <div className="top-suppliers flex flex-col gap-5">
           <h2>Top Suppliers</h2>
-          <ul>
-            {stats.topSuppliers.map((s) => (
-              <li key={s.id}>
-                <h3>{s.name}</h3>
-                <p>{s.totalOrders} Orders</p>
-              </li>
-            ))}
+          <ul className="top-list-admin flex flex-col gap-2 p-4">
+            <li className="flex justify-between">
+              <h3>Supplier</h3>
+              <p>Orders</p>
+            </li>
+            <div>
+              {stats.topSuppliers.map((s) => (
+                <li key={s.id} className="flex justify-between">
+                  <h3>{s.name}</h3>
+                  <p>{s.totalOrders}</p>
+                </li>
+              ))}
+            </div>
           </ul>
         </div>
-        <div className="top-services flex flex-col gap-2">
+        <div className="top-services flex flex-col gap-5">
           <h2>Top Services</h2>
-          <ul>
-            {stats.topServices.map((s) => (
-              <li key={s.id}>
-                <h3>{s.name}</h3>
-                <p>{s.totalOrders} Orders</p>
-              </li>
-            ))}
+          <ul className="top-list-admin flex flex-col gap-2 p-4">
+            <li className="flex justify-between">
+              <h3>Service</h3>
+              <p>Orders</p>
+            </li>
+            <div>
+              {stats.topServices.map((s) => (
+                <li key={s.id} className="flex justify-between">
+                  <h3>{s.name}</h3>
+                  <p>{s.totalOrders}</p>
+                </li>
+              ))}
+            </div>
           </ul>
         </div>
       </div>
---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Card = ({ title, value }: { title: string; value: number }) => {
+  return (
+    <div className="dash-card-admin flex flex-col">
+      <h3 className="text-lg font-normal">{title}</h3>
+      <h4 className="text-lg py-1 whitespace-nowrap font-bold">{value}</h4>
+    </div>
+  );
+};
