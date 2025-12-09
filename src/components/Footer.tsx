707618bb392b6694257ac364f014bb8b65b838d9
ArcidesFerrao <cidesferrao@gmail.com>
2025-12-09 16:51:30 +0200

    footer copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="absolute bottom-0 items-center text-center">
-      <p className="font-thin text-xs">2025 Inventory App</p>
+      <p className="font-thin text-xs">© 2025 Contela Platform</p>
     </footer>
   );
 };
