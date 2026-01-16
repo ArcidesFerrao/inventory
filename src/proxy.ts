6163e30ff48dc9111b52716904359b0685cfb754
ArcidesFerrao <cidesferrao@gmail.com>
2026-01-16 20:49:23 +0200

    session instead of getToken

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,13 +1,17 @@
-import { getToken } from "next-auth/jwt";
+// import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth";
 
 
 
 export async function proxy(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
+    // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
+
+    const session = await auth();
+
 
     
-    if (!token) {
+    if (!session?.user) {
         return NextResponse.redirect(new URL("/login", req.url));
     }
   
